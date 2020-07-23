5141685198d75bd9d47edb286a008b88dcb15b90
ayonalmamun <ayonalmamun@gmail.com>
2020-07-24 00:53:00 +0600

    CRUD

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors'); // optional styling
+const connectDB = require('./config/db');
+// load env vars
+dotenv.config({ path: './config/config.env' });
+
+//connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, Promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
