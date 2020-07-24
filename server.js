18bb24cf0f2f8b89f7543a80979f66de78746d6c
ayonalmamun <ayonalmamun@gmail.com>
2020-07-25 03:33:29 +0600

    error handling part 1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors'); // optional styling
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 // load env vars
 dotenv.config({ path: './config/config.env' });
@@ -26,6 +27,9 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
+
+// must writer after mounting routers
+app.use(errorHandler);
 
 const server = app.listen(
   PORT,
